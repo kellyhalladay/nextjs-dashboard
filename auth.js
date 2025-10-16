07da7caf86c2014de3673a64d748aaad3aae1c35
kellyhalladay <khalladay91@gmail.com>
2025-10-16 09:40:21 -0400

    Removing console.log messages used to help debug

--- auth.js
+++ auth.js
@@ -10,14 +10,11 @@ async function getUser(email) {
     const sql = neon(`${process.env.DATABASE_URL}`);
     const users = await sql`SELECT * FROM users WHERE email=${email}`;
 
-    console.log('🧠 Query result:', users);
-
     if (!users) {
       return null;
     }
     return users;
   } catch (error) {
-    console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
   }
 }
@@ -27,31 +24,24 @@ export const { auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
       async authorize(credentials) {
-        console.log('🟡 Incoming credentials:', credentials);
-
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
 
         if (!parsedCredentials.success) {
-          console.log('⚠️ Invalid credentials format');
           return null;
         }
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
-          console.log('🟢 User from DB:', user);
 
           if (!user) {
-            console.log('❌ No user found');
             return null;
           }
           const passwordsMatch = await bcrypt.compare(password, user[0].password);
-          console.log('🔵 Password match?', passwordsMatch);
 
           if (passwordsMatch) {
-            console.log('✅ Login success!');
             return {
               id: user.id,
               name: user.name,
